0cbc075bc2aad6b500e42e70e569a0ad7130dec4
zildevvn <vuongdevvn0509@gmail.com>
2026-09-03 16:01:51 +0700

    add analytics

--- header.php
+++ header.php
@@ -8,8 +8,16 @@
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <?php wp_head(); ?>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-45WEF1FBMT"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-45WEF1FBMT');
+    </script>
 </head>
 
 <body id="body" <?php body_class(); ?>>
