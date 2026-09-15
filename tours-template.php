8c3a9ce75187d4d737873ae3b826f1aaf0566331
zildevvn <vuongdevvn0509@gmail.com>
2026-09-15 11:44:50 +0700

    improve template tours

--- tours-template.php
+++ tours-template.php
@@ -6,6 +6,7 @@ get_header();
 ?>
 <main id="primary" class="site-main">
     <?php get_template_part('template-parts/tours/hero-section'); ?>
+    <?php get_template_part('template-parts/tours/about-section'); ?>
     <?php get_template_part('template-parts/tours/tours-list'); ?>
 </main>
 <?php get_footer(); ?>
