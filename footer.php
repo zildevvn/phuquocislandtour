11bdb4c27afd690198d9517125f2fb72777420e1
zildevvn <vuongdevvn0509@gmail.com>
2026-09-22 10:51:23 +0700

    add app chat

--- footer.php
+++ footer.php
@@ -11,6 +11,19 @@ do_action('vm_hook_footer');
 do_action('vm_hook_search');
 wp_footer();
 ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/6ab1f74abad415344a348c78/1k33iruqh';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
